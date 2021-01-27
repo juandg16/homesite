174fc624d348588213899b7e7f33241b880ccbf1
Juan <juandg16@gmail.com>
2021-01-26 19:13:11 -0600

    allow submitting form to api gateway, saving to db

--- src/pages/Home/Title.js
+++ src/pages/Home/Title.js
@@ -2,41 +2,41 @@ import './Title.css';
 
 function Title() {
   return (    
-    <div class="header-section">
-        <div class="strip strip-neutral"></div>
-        <div class="white-strip" id="cover1" style={{zIndex: 5}}></div>
-        <div class="strip strip-contrast"></div>
-        <div class="white-strip" id="cover2" style={{zIndex: 4}}></div>
-        <div class="strip strip-light"></div>
-        <div class="white-strip" id="cover3" style={{zIndex: 3}}></div>
-        <div class="strip strip-primary"></div>
-        <div class="white-strip" id="cover4" style={{zIndex: 2}}></div>
-        <div class="strip strip-dark"></div>
-        <div class="white-strip" id="cover5" style={{zIndex: 1}}></div>
-        <div class="skew-clip clip-black">
-            <header class="header-black">
-                <h1 class="header-title title-black">
+    <div className="header-section">
+        <div className="strip strip-neutral"></div>
+        <div className="white-strip" id="cover1" style={{zIndex: 5}}></div>
+        <div className="strip strip-contrast"></div>
+        <div className="white-strip" id="cover2" style={{zIndex: 4}}></div>
+        <div className="strip strip-light"></div>
+        <div className="white-strip" id="cover3" style={{zIndex: 3}}></div>
+        <div className="strip strip-primary"></div>
+        <div className="white-strip" id="cover4" style={{zIndex: 2}}></div>
+        <div className="strip strip-dark"></div>
+        <div className="white-strip" id="cover5" style={{zIndex: 1}}></div>
+        <div className="skew-clip clip-black">
+            <header className="header-black">
+                <h1 className="header-title title-black">
                     Hello, I'm a full stack developer
                 </h1>
             </header>
         </div>
-        <div class="skew-clip noclip clip-black">
-            <p class="header-button-container button-container-black">
-                <button class="header-button button-black" id="header-button-black">
+        <div className="skew-clip noclip clip-black">
+            <p className="header-button-container button-container-black">
+                <button className="header-button button-black" id="header-button-black">
                     Juan Gutierrez
                 </button>
             </p>
         </div>
-        <div class="skew-clip clip-white">
-            <header class="header-white">
-                <h1 class="header-title title-white">
+        <div className="skew-clip clip-white">
+            <header className="header-white">
+                <h1 className="header-title title-white">
                     Hello, I'm a full-stack developer
                 </h1>
             </header>
         </div>
-        <div class="skew-clip noclip clip-white">
-            <p class="header-button-container button-container-white">
-                <button class="header-button button-white" id="header-button-white">
+        <div className="skew-clip noclip clip-white">
+            <p className="header-button-container button-container-white">
+                <button className="header-button button-white" id="header-button-white">
                     Juan Gutierrez
                 </button>
             </p>
--- src/pages/TIForm/TIForm.js
+++ src/pages/TIForm/TIForm.js
@@ -1,29 +1,108 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+import uuid from 'react-uuid';
 import './TIForm.css';
 
+const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      ...state,
+      type: '',
+      phase: '',
+      id: uuid(),
+    }
+  }
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+ }
+
 function TIForm() {
+  const [formData, setFormData] = useReducer(formReducer, {
+    gameId: uuid(),    
+    id: uuid(),
+    data: 'filler',
+  });
   const [submitting, setSubmitting] = useState(false);
+
   const handleSubmit = event => {
     event.preventDefault();
     setSubmitting(true);
+    
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    formData.timestamp = new Date().toISOString();
+    let raw = JSON.stringify(formData);
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
 
-   setTimeout(() => {
-     setSubmitting(false);
-   }, 3000)
+    fetch(process.env.REACT_APP_WRITE_API_URL, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      let parsedResult = JSON.parse(result);
+      if (parsedResult.statusCode === 200){
+        console.log(parsedResult.body);
+        setFormData({
+          reset: true
+        });
+      }
+      setSubmitting(false);
+    })
+  }
+
+  const handleChange = event => {
+   const isCheckbox = event.target.type === 'checkbox';
+   setFormData({
+     name: event.target.name,
+     value: isCheckbox ? event.target.checked : event.target.value,
+   })
   }
 
   return (    
-    <div class="ti-form">
+    <div className="ti-form">
         <h1>Form starts here</h1>
-        {submitting && <div>Submtting Form...</div>}
+        {submitting && 
+        <div>
+          You are submitting the following: 
+          <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+           <li>{process.env.REACT_APP_WRITE_API_URL}</li>
+         </ul>
+        </div>
+        }
         <form onSubmit={handleSubmit}>
-          <fieldset>
+          <fieldset disabled={submitting}>
             <label>
               <p>Type</p>
-              <input name="type" />
+              <select name="type" onChange={handleChange} value={formData.type || ''}>
+                  <option value="">--Please choose an option--</option>
+                  <option value="score">Score a point</option>
+                  <option value="trade">Finalize a trade</option>
+                  <option value="tactical">Tactical action</option>
+                  <option value="strategic">Strategic action</option>
+                  <option value="component">Component action</option>
+              </select>
             </label>
           </fieldset>
-          <button type="submit">Submit</button>
+          <fieldset disabled={submitting}>
+            <label>
+              <p>Phase</p>
+              <select name="phase" onChange={handleChange} value={formData.phase || ''}>
+                  <option value="">--Please choose an option--</option>
+                  <option value="strategy">Strategy</option>
+                  <option value="action">Action</option>
+                  <option value="status">Status</option>
+                  <option value="agenda">Agenda</option>
+              </select>
+            </label>
+          </fieldset>
+          <button type="submit" disabled={submitting}>Submit</button>
         </form>
     </div>
   );
